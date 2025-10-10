1caa91434b69f1f890398b30a854de5736db246c
Nancy Mayek <nancy.torresag@alumno.buap.mx>
2025-10-10 17:04:53 -0600

    Archivos de microservicios

---
+++ GESTION DE HOTELES/msv-habitaciones/src/test/java/com/example/habitaciones/MsvHabitacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habitaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvHabitacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
