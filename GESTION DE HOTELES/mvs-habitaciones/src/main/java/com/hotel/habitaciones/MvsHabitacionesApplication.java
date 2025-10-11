59c94a6d50078f49d1be5899d055098ddb55b5e1
Nancy Mayek <nancy.torresag@alumno.buap.mx>
2025-10-10 18:11:32 -0600

    Fixed:Paquetes y se añadio commons

---
+++ GESTION DE HOTELES/mvs-habitaciones/src/main/java/com/hotel/habitaciones/MvsHabitacionesApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.habitaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsHabitacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvsHabitacionesApplication.class, args);
+	}
+
+}
---
+++ GESTION DE HOTELES/mvs-huespedes/src/main/java/com/hotel/huespedes/MvsHuespedesApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.huespedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsHuespedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvsHuespedesApplication.class, args);
+	}
+
+}
