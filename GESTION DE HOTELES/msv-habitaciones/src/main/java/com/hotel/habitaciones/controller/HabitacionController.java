6dfb82001f1def48385db30564a67137a8d4b976
Nancy Mayek <nancy.torresag@alumno.buap.mx>
2025-10-13 14:56:30 -0600

    fix: Habitacion Repository

--- GESTION DE HOTELES/msv-habitaciones/src/main/java/com/hotel/habitaciones/controller/HabitacionController.java
+++ GESTION DE HOTELES/msv-habitaciones/src/main/java/com/hotel/habitaciones/controller/HabitacionController.java
@@ -1,11 +1,14 @@
 package com.hotel.habitaciones.controller;
 
 
+import org.springframework.web.bind.annotation.RestController;
+
 import com.hotel.commons.controllers.CommonController;
 import com.hotel.commons.dto.HabitacionRequest;
 import com.hotel.commons.dto.HabitacionResponse;
 import com.hotel.habitaciones.services.HabitacionService;
 
+@RestController
 public class HabitacionController extends CommonController<HabitacionRequest,HabitacionResponse, HabitacionService>{
 
 	public HabitacionController(HabitacionService service) {
--- GESTION DE HOTELES/msv-habitaciones/src/main/java/com/hotel/habitaciones/services/HabitacionServiceImpl.java
+++ GESTION DE HOTELES/msv-habitaciones/src/main/java/com/hotel/habitaciones/services/HabitacionServiceImpl.java
@@ -3,6 +3,7 @@ package com.hotel.habitaciones.services;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.hotel.commons.dto.HabitacionRequest;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @Slf4j
 public class HabitacionServiceImpl implements HabitacionService{
+
 	private final HabitacionRepository habitacionRepository;
 	private final HabitacionMapper habitacionMapper;
 	
@@ -39,7 +41,12 @@ public class HabitacionServiceImpl implements HabitacionService{
 
 	@Override
 	public HabitacionResponse insertar(HabitacionRequest request) {
-		log.info("Insertando nuevo Producto con numero {}", request.numero());
+		log.info("Insertando nueva Habitacion con numero {}", request.numero());
+		
+		 if (habitacionRepository.existsByNumero(request.numero())) {
+			 throw new DataIntegrityViolationException("Ya existe una habitación con el número: " + request.numero());
+	        }
+		
 		return habitacionMapper.entityToResponse(
 				habitacionRepository.save(habitacionMapper.requestToEntity(request))
 		);
@@ -55,14 +62,14 @@ public class HabitacionServiceImpl implements HabitacionService{
 		habitacion.setDescripcion(request.descripcion());
 		habitacion.setPrecio(request.precio());
 		habitacion.setCapacidad(request.capacidad());
-		habitacion.setTipo(request.idEstado());
+		habitacion.setIdEstado(request.idEstado());;
 		
 		return habitacionMapper.entityToResponse(habitacionRepository.save(habitacion));
 	}
 
 	@Override
 	public void eliminar(Long id) {
-		log.info("Eliminando Producto con id {}", id);
+		log.info("Eliminando Habitacion con id {}", id);
 		Habitacion habitacion = getHabitacionOrThrow(id);
 		habitacionRepository.delete(habitacion);
 		log.info("Numero de Habitacion eliminada: {}", habitacion.getNumero());
@@ -70,7 +77,7 @@ public class HabitacionServiceImpl implements HabitacionService{
 	
 	private Habitacion getHabitacionOrThrow(Long id) {
         return habitacionRepository.findById(id).orElseThrow(() -> 
-            new NoSuchElementException("Producto no encontrado con el id: " + id)
+            new NoSuchElementException("Habitacion no encontrado con el id: " + id)
         );
     }
 
