7d334abdc32c9569b029a595a59fe7375615c050
Guillermo Izalde <izalde.guillermo45@gmail.com>
2025-10-13 22:43:38 -0600

    fix request y response Huesped

--- GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/dto/HuespedRequest.java
+++ GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/dto/HuespedRequest.java
@@ -5,30 +5,30 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public class HuespedRequest {
-	
-	@NotBlank(message = "El nombre es obligatorio.")
-    @Size(min = 10, message = "El nombre debe tener al menos 10 caracteres.")
-    private String nombre;
+public record HuespedRequest (
+		@NotBlank(message = "El nombre es obligatorio.")
+	    @Size(min = 10, message = "El nombre debe tener al menos 10 caracteres.")
+	    String nombre,
 
-    @NotBlank(message = "El apellido es obligatorio.")
-    @Size(min = 10, message = "El apellido debe tener al menos 10 caracteres.")
-    private String apellido;
+	    @NotBlank(message = "El apellido es obligatorio.")
+	    @Size(min = 10, message = "El apellido debe tener al menos 10 caracteres.")
+	    String apellido,
 
-    @NotBlank(message = "El email es obligatorio.")
-    @Email(message = "Debe ingresar un correo electrónico válido.")
-    private String email;
+	    @NotBlank(message = "El email es obligatorio.")
+	    @Email(message = "Debe ingresar un correo electrónico válido.")
+	    String email,
+	    
+	    @NotBlank(message = "El teléfono es obligatorio.")
+	    @Pattern(regexp = "^[0-9]{10}$", message = "El teléfono debe tener exactamente 10 dígitos.")
+	    String telefono,
 
-    @NotBlank(message = "El teléfono es obligatorio.")
-    @Pattern(regexp = "^[0-9]{10}$", message = "El teléfono debe tener exactamente 10 dígitos.")
-    private String telefono;
+	    @NotBlank(message = "El documento es obligatorio.")
+	    @Pattern(regexp = "^(INE|Pasaporte)$", message = "El documento debe ser 'INE' o 'Pasaporte'.")
+	    Long idDocumento,
 
-    @NotBlank(message = "El documento es obligatorio.")
-    @Pattern(regexp = "^(INE|Pasaporte)$", message = "El documento debe ser 'INE' o 'Pasaporte'.")
-    private String documento;
-
-    @NotBlank(message = "La nacionalidad es obligatoria.")
-    @Size(max = 50, message = "La nacionalidad no debe exceder los 50 caracteres.")
-    private String nacionalidad;
-
+	    @NotBlank(message = "La nacionalidad es obligatoria.")
+	    @Size(max = 50, message = "La nacionalidad no debe exceder los 50 caracteres.")
+	    String nacionalidad
+)
+{	
 }
--- GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/dto/HuespedResponse.java
+++ GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/dto/HuespedResponse.java
@@ -1,13 +1,15 @@
 package com.hotel.commons.dto;
 
-public class HuespedResponse {
-	
-	private Long id;
-    private String nombre;
-    private String apellido;
-    private String email;
-    private String telefono;
-    private String documento;
-    private String nacionalidad;
+public record HuespedResponse (
+		
+		Long id,
+	     String nombre,
+	     String apellido,
+	     String email,
+	     String telefono,
+	     Long idDocumento,
+	     String nacionalidad
+	     
+	     ) 
 
-}
+{}
