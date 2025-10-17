e4437081caf67d2c5541cd86ca3163e0846a4dc2
Nancy Mayek <nancy.torresag@alumno.buap.mx>
2025-10-16 20:50:26 -0600

    Fix: Feighn clients for Habitacion, huespedes and reservas

--- GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/clients/HuespedClient.java
+++ GESTION DE HOTELES/commons/src/main/java/com/hotel/commons/clients/HuespedClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.hotel.commons.dto.HuespedResponse;
 
-@FeignClient(name = "msv-huesped")
+@FeignClient(name = "msv-huespedes")
 public interface HuespedClient {
 	@GetMapping("/{id}")
 	HuespedResponse obtenerHuespedPorId(@PathVariable Long id);
